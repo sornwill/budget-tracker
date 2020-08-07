5d8b613af9b51d57929666dc9c6d60d584b55302
Soul <sornwilliam@gmail.com>
2020-08-06 20:25:54 -0500

    Wasn't the solution previously but now service worker magically works

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,20 @@
 const FILES_TO_CACHE = [
     '/',
     '/index.html',
+    '/manifest.webmanifest',
+    '/assets/icons/icon-192x192.png',
+    '/assets/icons/icon-512x512.png',
     '/assets/js/index.js',
     '/assets/js/db.js',
-    '/manifest.webmanifest',
-    '/assets/css/style.css','/assets/icons/icon-192x192.png',
-    '/assets/icons/icon-512x512.png'
+    '/assets/css/style.css'
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache";
+const DATA_CACHE_NAME = "data-cache";
 
-
-self.addEventListener("install", function (evt) {
+self.addEventListener('install', function (evt) {
     evt.waitUntil(
-        cache.open(CACHE_NAME).then(cache => {
+        caches.open(CACHE_NAME).then(cache => {
             console.log("Your files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -25,12 +25,12 @@ self.addEventListener("install", function (evt) {
 
 self.addEventListener("activate", function (evt) {
     evt.waitUntil(
-        cache.keys().then(keyList => {
+        caches.keys().then(keyList => {
             return Promise.all(
                 keyList.map(key => {
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                         console.log("Removing old cache data", key);
-                        return cache.delete(key);
+                        return caches.delete(key);
                     }
                 })
             );
@@ -40,33 +40,34 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
+self.addEventListener('fetch', function (evt) {
+    if(evt.request.url.includes('api/')) {
+        console.log('[Service Worker] Fetch (data)', evt.request.url);
 
-self.addEventListener("fetch", function (evt) {
-
-    if (evt.request.url.includes("/api/")) {
         evt.respondWith(
-            cache.open(DATA_CACHE_NAME).then(cache => {
+            caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
-                    .then(response => {
-                     
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
-
-                        return response;
-                    })
-                    .catch(err => {
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(evt.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch(err => {
+                    return cache.match(evt.request);
+                });
+            })
         );
 
         return;
     }
 
     evt.respondWith(
-        cache.match(evt.request).then(function (response) {
-            return response || fetch(evt.request);
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.match(evt.request).then(response => {
+                return response || fetch(evt.request);
+            });
         })
     );
+
 });
